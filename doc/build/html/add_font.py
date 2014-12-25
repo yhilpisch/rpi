431c11f86a8ce5e47336d2cf858ca9a81da6c3ca
yves <hilpisch@visixion.com>
2014-12-25 18:35:37 +0100

    First published version.

---
+++ doc/build/html/add_font.py
@@ -0,0 +1,18 @@
+import os
+
+to_add = "<link href='http://fonts.googleapis.com/css?family=PT+Sans' "
+to_add += "rel='stylesheet' type='text/css'>\n"
+
+
+files = os.listdir('.')
+
+for f in files:
+    if f.endswith('.html') and f[0] in ['0', 'i']:
+        os.rename(f, f + '_old')
+        r = open(f + '_old', 'r').readlines()
+        r = [l.replace('65p6tn4p8i', 'token') for l in r]
+        n = open(f, 'w')
+        n.writelines(r[:12])
+        n.write(to_add)
+        n.writelines(r[12:])
+        n.close()
